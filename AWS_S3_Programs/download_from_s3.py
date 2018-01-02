5c1ab8447bbf66adb5c3f6d26567935885fe1444
Spiro Ganas <spiroganas@gmail.com>
2018-01-02 09:56:47 -0500

    began working on script to download S3 folders

---
+++ AWS_S3_Programs/download_from_s3.py
@@ -0,0 +1,12 @@
+# Spiro Ganas
+# 1/2/18
+#
+# Downloads a folder or bucket from AWS S3
+
+
+
+
+
+def download_folder_from_s3(bucket: str ='', folder: str ='', local_folder: str = ''):
+    '''Downloads a folder or an entire bucket from AWS S2 and saves the files in local_folder'''
+    pass
